4bdc897ccf602b63461e8b2e4dfa942c3207b3b0
Wanming Lin <wanming.lin@intel.com>
2021-12-02 09:23:29 +0800

    Override path of wasm files

--- semantic_segmentation/deeplabv3_mnv2_onnx.js
+++ semantic_segmentation/deeplabv3_mnv2_onnx.js
@@ -18,11 +18,19 @@ export class DeepLabV3MNV2ONNX {
   }
 
   async load() {
-  // Create the model runner with the model.
-  const model = './models/deeplab_mobilenetv2_321_no_argmax.onnx';
-  const session = ort.InferenceSession.create(model, {
-      executionProviders: ['wasm', {name: 'webnn', devicePreference: 1}], logSeverityLevel: 0 });
-  return session;
+    // override path of wasm files - for each file
+    ort.env.wasm.wasmPaths = {
+      'ort-wasm.wasm': location.href + '/onnxruntime-web/js/web/dist/ort-wasm.wasm',
+      'ort-wasm-simd.wasm': location.href + '/onnxruntime-web/js/web/dist/ort-wasm-simd.wasm',
+      'ort-wasm-threaded.wasm': location.href + '/onnxruntime-web/js/web/dist/ort-wasm-threaded.wasm',
+      'ort-wasm-simd-threaded.wasm': location.href + '/onnxruntime-web/js/web/dist/ort-wasm-simd-threaded.wasm'
+    };
+    ort.env.wasm.numThreads = 1;
+    // Create the model runner with the model.
+    const model = './models/deeplab_mobilenetv2_321_no_argmax.onnx';
+    const session = ort.InferenceSession.create(model, {
+        executionProviders: ['wasm', {name: 'webnn', devicePreference: 1}], logSeverityLevel: 0 });
+    return session;
   }
 
   async compute(session, inputData) {
