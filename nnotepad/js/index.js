c9cfa376f90f568109247bf3a9fd49a4bfed28b6
Honry <wanming.lin@intel.com>
2024-06-11 01:52:53 +0000

    Deploying to gh-pages from @ Honry/webnn-samples@919aa9f55cc0d9871a0189d1fdf6b35123b08da6 🚀

--- nnotepad/js/index.js
+++ nnotepad/js/index.js
@@ -54,11 +54,34 @@ document.addEventListener('DOMContentLoaded', async (e) => {
   $$('dialog > button').forEach((e) => e.addEventListener('click', (e) => {
     e.target.parentElement.close();
   }));
-  $$('dialog').forEach((e) => e.addEventListener('close', (e) => {
-    $('#input').focus();
-  }));
+  $$('dialog').forEach((dialog) => {
+    dialog.addEventListener('click', (e) => {
+      const rect = e.target.getBoundingClientRect();
+      if (e.clientY < rect.top || e.clientY > rect.bottom ||
+          e.clientX < rect.left || e.clientX > rect.right) {
+        e.target.close();
+      }
+    });
+    dialog.addEventListener('close', (e) => {
+      $('#input').focus();
+    });
+  });
   $('#peek').addEventListener('click', (e) => $('#srcDialog').showModal());
   $('#help').addEventListener('click', (e) => $('#helpDialog').showModal());
+
+  $('#resize').addEventListener('pointerdown', (e) => {
+    const resize = e.target;
+    resize.setPointerCapture(e.pointerId);
+    const listener = (e) => {
+      document.documentElement.style.setProperty(
+          '--input-height', `${e.clientY}px`);
+    };
+    resize.addEventListener('pointermove', listener);
+    resize.addEventListener('pointerup', () => {
+      resize.releasePointerCapture(e.pointerId);
+      resize.removeEventListener('pointermove', listener);
+    }, {once: true});
+  });
 });
 
 function explain(outputs) {
@@ -79,7 +102,7 @@ function explain(outputs) {
 
     const width = [...buffer]
         .map((n) => String(n).length)
-        .reduce((a, b) => Math.max(a, b));
+        .reduce((a, b) => Math.max(a, b), 0);
 
     const out = [];
     let bufferIndex = 0;
@@ -95,10 +118,7 @@ function explain(outputs) {
         if (i !== shape[dim] - 1) {
           out.push(', ');
           if (dim + 1 !== shape.length) {
-            if (dim + 2 !== shape.length) {
-              out.push('\n');
-            }
-            out.push('\n');
+            out.push('\n'.repeat(shape.length - dim - 1));
             out.push(' '.repeat(indent + dim + 1));
           }
         }
